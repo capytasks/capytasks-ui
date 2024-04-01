e25750bb87928d5a169f2d1d553bcd252dee709b
Janna <litvinovajann@gmail.com>
2024-04-01 09:04:37 -0700

    fix: fix the routes logic

---
+++ src/components/Layout.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import { Outlet } from "react-router-dom"
+import Header from "./Header"
+import Footer from "./Footer"
+
+const Layout: React.FC = () => {
+    return (
+        <div className="app">
+            <Header />
+                <main>
+                    <Outlet />
+                </main>
+            <Footer />
+        </div>
+    )
+    }
+    export default Layout;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+// pages/Login.tsx
+import React, { useState } from 'react';
+import '../App.css'; 
+
+const Home: React.FC = () => {
+return (
+    <div>
+        Home Page
+    </div>
+)
+}
+export default Home;
